5306fa9479feca12b0f414095715215527fe3724
Min Shan <liewminshan@gmail.com>
2022-03-01 22:30:52 +0800

    update masterg

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename="cra-test">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
